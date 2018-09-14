68a8ca17de900172b0ac384d2f69e86b207aa272
YaloWan <1648332121@qq.com>
2018-09-14 09:41:16 +0800

    MyProject

---
+++ js/index.js
@@ -0,0 +1,28 @@
+$(function(){
+	var img_length = $('.banner_img').length; //图片的张数
+	var i  = 1;
+ 
+	function banner(){ //轮播函数
+		if(i < 0){
+			i = img_length - 1;
+		}
+		if(i > img_length-1){
+			i = 0;
+		}
+		$('.banner_images').animate({'left': (-i*260)+'px'});//往左移，移动的距离等于图片的宽度。
+		$('.points li').eq(i).addClass('point_on').siblings().removeClass();
+		i++;
+	}
+	var banner_timer = setInterval(banner, 5000);
+ 
+	$('.switcher p').hover(function(){	//鼠标移入左右切换箭头清除定时器使轮播停止
+		clearInterval(banner_timer);
+	},function(){	//鼠标移出切换箭头还原定时器，这里为避免定时器重复 还将定时器赋值给上边定义的banner_timer变量
+		banner_timer = setInterval(banner, 5000);
+	});
+
+	$('.points li').click(function(){	//点击相应的焦点跳转到对应图片
+		i = $(this).index();
+		banner(); 
+	});
+});
--- js/xSlider.js
+++ js/xSlider.js
@@ -1,4 +1,4 @@
-;(function($){
+(function($){
 
   var xSlider = function(el, userConfig) {
 
